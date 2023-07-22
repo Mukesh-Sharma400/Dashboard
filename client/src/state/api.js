ba8b84ec884de39659918f07ce8a1d3e6adf9fc1
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-07-22 15:23:03 +0530

    api changed

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://dashboard-backend-x5nm.onrender.com",
+  }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
